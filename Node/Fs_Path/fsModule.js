cec393ff538322b5dcd88e2a6312f66b730893c9
Dev120604 <123566726+Dev120604@users.noreply.github.com>
2025-05-24 19:17:06 +0530

    commit

--- Node/Fs_Path/fsModule.js
+++ Node/Fs_Path/fsModule.js
@@ -1,15 +1,30 @@
-const fs = require("fs");
+// const fs = require("fs");
+//
+// console.log("Start");
+//
+// fs.writeFile("demo.txt","This is demo.txt",(err)=>{
+//     if(err) throw err;
+//     console.log("File Written");
+//
+//     fs.readFile("demo.txt",(err,data)=>{
+//         if(err) throw err;
+//         console.log("Data :",data.toString());
+//     });
+// });
+// console.log("End");
+//
+//
+// fs.appendFile("demo.txt","Appended Text",(err)=>{
+//     if(err) throw err;
+//     console.log("Appended Successfully");
+// });
+//
+// fs.readFile("demo.txt",(err,data)=>{
+//     if(err) throw err;
+//     console.log("After Appending :",data.toString());
+// });
+//
 
-console.log("Start");
+import fs from 'fs/promises';
 
-fs.writeFile("demo.txt","This is demo.txt",(err)=>{
-    if(err) throw err;
-    console.log("File Written");
 
-    fs.readFile("demo.txt",(err,data)=>{
-        if(err) throw err;
-        console.log("Data : "+data.toString());
-    });
-});
-
-console.log("End");
